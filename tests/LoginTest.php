c8e2d1531a73cb522d8dbea9d1e33f186451703f
WALTERGT04 <72637876+WALTERGT04@users.noreply.github.com>
2020-11-10 19:37:38 -0600

    Update LoginTest.php

--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -8,7 +8,7 @@ final class LoginTest extends TestCase
     {
         $this->assertEquals(
             true,
-            Login::logearse('admin','admin')
+            Login::logearse('admin2','admin')
         );
     }
 }
